7d041c2d9411b915bab7ad1c844b67a3c6b6aa0a
Igor <smoke-lmt@mail.ru>
2021-12-01 15:08:46 +0300

    начал писать работу с флеш

---
+++ dev/Inc/flash_data.h
@@ -0,0 +1,8 @@
+/*
+ * flash_data.h
+ *
+ *  Created on: Dec 1, 2021
+ *      Author: igor.dymov
+ */
+
+
---
+++ dev/Src/flash_data.c
@@ -0,0 +1,8 @@
+/*
+ * flash_data.c
+ *
+ *  Created on: Dec 1, 2021
+ *      Author: igor.dymov
+ */
+
+
